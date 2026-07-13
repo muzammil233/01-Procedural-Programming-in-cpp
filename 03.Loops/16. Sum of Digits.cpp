29a3a45d23e39c0abf2eea8dcbdef35de322af19
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-07-13 13:09:14 +0500

    Add files via upload

---
+++ 03.Loops/16. Sum of Digits.cpp
@@ -0,0 +1,29 @@
+// Sum of Digits.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    
+    int count = 0;
+    
+    if(n==0)
+    {
+        count = 1;
+    }
+    else
+    {
+        while(n!=0)
+    {
+        n = n / 10;
+        count++;
+     }
+     }
+     cout<<"Number of digits: "<<count;
+     
+     return 0;
+        
+}    
