1ffcce5c8290c99a473cc81b08725335816be4f0
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-08-14 11:40:56 +0500

    Add files via upload

---
+++ 03.Loops/25. Prime factors.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    
+    for(int i=2; i<=n ; i++)
+    {
+        while(n%i==0)
+        {
+            cout<<i<<" ";
+            n = n / i;
+        }   
+     }
+     return 0;
+}                
