7e479808313987c68d8b19133d07cc05ec29c328
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-08-21 21:58:43 +0500

    Add files via upload

---
+++ 04. Functions/03. Even odd check.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool isEven(int n)
+{
+    if(n%2==0)
+    
+        return true;
+    
+      return false;
+ }
+ 
+ int main()
+ {
+     int num;
+     cout<<"Enter a number : ";
+     cin>>num;
+     
+     if(isEven(num))
+     {
+         cout<<num<<" is even.";
+      }
+      else
+      {
+          cout<<num<<" is odd.";
+       }
+       
+   return 0;
+ }               
